c4b055e84d33c499179fb0a4d516c0edf6b275ac
M-Feale <marjolainefeale@gmail.com>
2023-11-23 15:26:10 -0500

    implemented-100%-test-coverage-handleKeyPress

--- __tests__/handleKeyPress.test.js
+++ __tests__/handleKeyPress.test.js
@@ -9,20 +9,63 @@ jest.spyOn(document, `getElementById`).mockImplementation((id) => id)
 
 describe(`handleKeyPress`, () => {
     test("when Numpad1 is pressed, it should call animate and play sound ", () => {
-    
+
         const handleAnimation = require("../handleAnimation");
         const handleKeyPress = require("../handleKeyPress");
         const playSound = require("../playSound");
 
-        const mockEvent = {
-            code: "Numpad1",
-            keyId: `key-1`,
-            soundName: `chirp`
-        }
+        const mockEvents = [
+            {
+                code: "Numpad1",
+                keyId: "key-1",
+                soundName: "chirp"
+            },
+            {
+                code: "Numpad2",
+                keyId: "key-2",
+                soundName: "clap"
+            },
+            {
+                code: "Numpad3",
+                keyId: "key-3",
+                soundName: "kick"
+            },
+            {
+                code: "Numpad4",
+                keyId: "key-4",
+                soundName: "laser"
+            },
+            {
+                code: "Numpad5",
+                keyId: "key-5",
+                soundName: "fuzz"
+            },
+            {
+                code: "Numpad6",
+                keyId: "key-6",
+                soundName: "tom"
+            },
+            {
+                code: "Numpad7",
+                keyId: "key-7",
+                soundName: "hit"
+            },
+            {
+                code: "Numpad8",
+                keyId: "key-8",
+                soundName: "metronome"
+            },
+            {
+                code: "Numpad9",
+                keyId: "key-9",
+                soundName: "whistle"
+            }
+        ]
 
-        handleKeyPress(mockEvent)
-
-        expect(handleAnimation).toHaveBeenCalledWith("key-1");
-        expect(playSound).toHaveBeenCalledWith("chirp")
+        mockEvents.forEach((mockEvent) => {
+            handleKeyPress(mockEvent)
+            expect(handleAnimation).toHaveBeenCalledWith(mockEvent.keyId);
+            expect(playSound).toHaveBeenCalledWith(mockEvent.soundName)
+        })
     })
 })
